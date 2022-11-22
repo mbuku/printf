ed0dbb5c792b970824a06a28aabd23e306450bae
mbuku <joysmbuku@gmail.com>
2022-11-22 06:28:28 -0800

    new committed file

---
+++ print_mod.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_mod - print %.
+ *
+ * @args: arguments.
+ *
+ * Return: count of chars.
+ */
+
+int print_mod(va_list args)
+
+{
+
+(void)args;
+
+_putchar('%');
+
+return (1);
+
+}
