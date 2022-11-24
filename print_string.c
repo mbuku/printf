04d7ae452fd7f3b2209731a3ea9ee62a775cd095
Joys mbuku <84926001+mbuku@users.noreply.github.com>
2022-11-24 10:14:05 +0100

    commit print_string.c

--- print_string.c
+++ print_string.c
@@ -11,27 +11,19 @@
  */
 
 int print_string(va_list args)
-
 {
+	int i;
+	int count_fun = 0;
+	char *str = va_arg(args, char *);
 
-int i;
+	if (!str)
+		str = "(null)";
 
-int count_fun = 0;
+	if (str[0] == '\0')
+		return (-1);
 
-char *str = va_arg(args, char *);
+	for (i = 0; str[i] != '\0'; i++)
+		count_fun += _putchar(str[i]);
 
-if (!str)
-
-str = "(null)";
-
-if (str[0] == '\0')
-
-return (-1);
-
-for (i = 0; str[i] != '\0'; i++)
-
-count_fun += _putchar(str[i]);
-
-return (count_fun);
-
+	return (count_fun);
 }
