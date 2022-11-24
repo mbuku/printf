eafaa20388007b9f7131600546b7b40af27c3661
Joys mbuku <84926001+mbuku@users.noreply.github.com>
2022-11-24 09:56:21 +0100

    Commit get_function.c

--- get_function.c
+++ get_function.c
@@ -10,53 +10,32 @@
  */
 
 int get_function(char con_spec, va_list args)
-
 {
+	int i = 0;
+	int count_fun = 0;
 
-int i = 0;
+	specifiers_t spec[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_mod},
+		{'d', print_digit},
+		{'i', print_digit},
+		{'r', print_rev_string},
+		{0, NULL}
+	};
 
-int count_fun = 0;
+	while (spec[i].specifiers)
+	{
+		if (con_spec == spec[i].specifiers)
+			count_fun += spec[i].f(args);
+		i++;
+	}
 
-specifiers_t spec[] = {
+	if (count_fun == 0)
+	{
+		count_fun += _putchar('%');
+		count_fun += _putchar(con_spec);
+	}
 
-{'c', print_char},
-
-{'s', print_string},
-
-{'%', print_mod},
-
-{'d', print_digit},
-
-{'i', print_digit},
-
-{'r', print_rev_string},
-
-{0, NULL}
-
-};
-
-while (spec[i].specifiers)
-
-{
-
-if (con_spec == spec[i].specifiers)
-
-count_fun += spec[i].f(args);
-
-i++;
-
-}
-
-if (count_fun == 0)
-
-{
-
-count_fun += _putchar('%');
-
-count_fun += _putchar(con_spec);
-
-}
-
-return (count_fun);
-
+	return (count_fun);
 }
