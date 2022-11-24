f300dc15559fe37928323d3178902c714d09c3d1
Joys mbuku <84926001+mbuku@users.noreply.github.com>
2022-11-24 10:06:58 +0100

    Commit main.h

--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
 
 /**
  * struct format - Struct for format
@@ -11,33 +13,21 @@
  */
 
 typedef struct specifiers
-
 {
-
-char specifiers;
-
-int (*f)(va_list);
-
+	char specifiers;
+	int (*f)(va_list);
 } specifiers_t;
 
 /*prototypes*/
-
 int _printf(const char *format, ...);
-
 int get_function(char s, va_list args);
-
 int _putchar(char c);
 
 /*Conversion specifiers*/
-
 int print_char(va_list args);
-
 int print_string(va_list args);
-
 int print_digit(va_list args);
-
 int print_mod(va_list args);
-
 int print_rev_string(va_list args);
 
 #endif
